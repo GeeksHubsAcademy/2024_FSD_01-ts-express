6111f8fad151256ab829d7179547cb8c9568faac
datata <dani@danitarazona.dev>
2024-02-23 11:18:56 +0100

    feat: helalthy endpoint

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,15 @@ const app: Application = express();
 
 const PORT = process.env.PORT || 4001;
 
+app.get('/healthy', (req, res) => {
+  res.status(200).json(
+    {
+      success: true,
+      message: "Server is healthy"
+    }
+  );
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 })
