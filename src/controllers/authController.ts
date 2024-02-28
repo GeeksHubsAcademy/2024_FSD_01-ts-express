a18fabcd8935d42404bb08b5d4fbe91eac0462ad
datata <dani@danitarazona.dev>
2024-02-28 12:00:41 +0100

    update: register user logic added

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/User";
 
-export const register = (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
   try {
     console.log(req.body);
     const email = req.body.email;
     const password = req.body.password;
+    const name = req.body.name;
 
     // const { email, password } = req.body
 
     // Validacion password
-    if( password.length < 6 || password.length > 10) {
+    if (password.length < 6 || password.length > 10) {
       return res.status(400).json({
         success: false,
         message: "Eres toooooooonto la contraseña tiene que estar entre 6 y 10 caracateres"
@@ -17,20 +20,36 @@ export const register = (req: Request, res: Response) => {
     }
 
     // Validacion email
-    const validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    if (!validEmail.test(email) ){
+    const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if (!validEmail.test(email)) {
       return res.status(400).json(
         {
           success: false,
           message: "format email invalid"
         }
       )
-    }  
-    
+    }
+
+    // tratamos la data si fuera necesario
+    const passwordEncrypted = bcrypt.hashSync(password, 8);
+    // comprobamos que se genara la contraseña encriptada
+    console.log(passwordEncrypted);
+
+    const newUser = await User.create({
+      name: name,
+      email: email,
+      password: passwordEncrypted,
+      role: {
+        id:1
+      }
+    }).save()
+
+
     res.status(201).json(
       {
         success: true,
-        message: 'User registered successfully'
+        message: 'User registered successfully',
+        data: newUser
       }
     )
   } catch (error) {
